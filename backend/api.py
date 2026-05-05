2b7cb7f2a8dd2e31d3de1c3b47dae656db1b5cf7
nikita <nikitaevtefeev@gmail.com>
2026-05-05 13:41:49 +0300

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -4,8 +4,7 @@ import time
 from fastapi import FastAPI, UploadFile, File
 import httpx
 from pydantic import BaseModel
-from html_parser import parse_law
-from parser import parse_document
+from .html_parser import parse_law
 from fastapi import FastAPI, Request, HTTPException, Depends
 import asyncio
 from fastapi.middleware.cors import CORSMiddleware
